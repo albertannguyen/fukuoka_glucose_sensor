af6bb7623c16880a3c2bf5a818e0d0d113123076
Albert Nguyen <albert.nguyen10.AN@gmail.com>
2025-04-05 07:19:55 -0500

    Update user_empty_peripheral_template.c
    I just cleaned up the comments and added other includes for GPIO functionality.

--- src/user_empty_peripheral_template.c
+++ src/user_empty_peripheral_template.c
@@ -1,59 +1,26 @@
-/**
- ****************************************************************************************
- *
- * @file user_empty_peripheral_template.c
- *
- * @brief Empty peripheral template project source code.
- *
- * Copyright (C) 2012-2023 Renesas Electronics Corporation and/or its affiliates.
- * All rights reserved. Confidential Information.
- *
- * This software ("Software") is supplied by Renesas Electronics Corporation and/or its
- * affiliates ("Renesas"). Renesas grants you a personal, non-exclusive, non-transferable,
- * revocable, non-sub-licensable right and license to use the Software, solely if used in
- * or together with Renesas products. You may make copies of this Software, provided this
- * copyright notice and disclaimer ("Notice") is included in all such copies. Renesas
- * reserves the right to change or discontinue the Software at any time without notice.
- *
- * THE SOFTWARE IS PROVIDED "AS IS". RENESAS DISCLAIMS ALL WARRANTIES OF ANY KIND,
- * WHETHER EXPRESS, IMPLIED, OR STATUTORY, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
- * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. TO THE
- * MAXIMUM EXTENT PERMITTED UNDER LAW, IN NO EVENT SHALL RENESAS BE LIABLE FOR ANY DIRECT,
- * INDIRECT, SPECIAL, INCIDENTAL OR CONSEQUENTIAL DAMAGES ARISING FROM, OUT OF OR IN
- * CONNECTION WITH THE SOFTWARE, EVEN IF RENESAS HAS BEEN ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGES. USE OF THIS SOFTWARE MAY BE SUBJECT TO TERMS AND CONDITIONS CONTAINED IN
- * AN ADDITIONAL AGREEMENT BETWEEN YOU AND RENESAS. IN CASE OF CONFLICT BETWEEN THE TERMS
- * OF THIS NOTICE AND ANY SUCH ADDITIONAL LICENSE AGREEMENT, THE TERMS OF THE AGREEMENT
- * SHALL TAKE PRECEDENCE. BY CONTINUING TO USE THIS SOFTWARE, YOU AGREE TO THE TERMS OF
- * THIS NOTICE.IF YOU DO NOT AGREE TO THESE TERMS, YOU ARE NOT PERMITTED TO USE THIS
- * SOFTWARE.
- *
- ****************************************************************************************
- */
-
-/**
- ****************************************************************************************
- * @addtogroup APP
- * @{
- ****************************************************************************************
- */
-
 /*
+ ****************************************************************************************
  * INCLUDE FILES
  ****************************************************************************************
  */
 
-#include "rwip_config.h"             // SW configuration
+// template
+#include "rwip_config.h" // SW configuration
 #include "gattc_task.h"
 #include "app_api.h"
 #include "user_empty_peripheral_template.h"
 
+// custom
+#include "gpio.h"
+#include "user_periph_setup.h"
+
 /*
+ ****************************************************************************************
  * FUNCTION DEFINITIONS
  ****************************************************************************************
 */
 
-
+// template
 void user_on_connection(uint8_t connection_idx, struct gapc_connection_req_ind const *param)
 {
     default_app_on_connection(connection_idx, param);
@@ -85,4 +52,4 @@ void user_catch_rest_hndl(ke_msg_id_t const msgid,
     }
 }
 
-/// @} APP
+// custom
