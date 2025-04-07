5ddd4b93a2b5a95e1d15a4a15bc2887c206091d6
Dhruv <dhruv_nellutla@tamu.edu>
2025-04-07 18:12:10 -0500

    Create GlucoseMonitorApp.swift

---
+++ GlucoseMonitorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Glucose_MonitorApp.swift
+//  Glucose Monitor
+//
+//  Created by Dhruv Nellutla on 3/31/25.
+//
+
+import SwiftUI
+
+@main
+struct Glucose_MonitorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
