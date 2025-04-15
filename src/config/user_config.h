f9cfe2ebd2fe155a55335d2a4e5b8c63b1b7d27f
Albert Nguyen <albert.nguyen10.AN@gmail.com>
2025-04-15 11:30:21 -0500

    Enabled sleep mode
    Does not break the ADC timer function

--- src/config/user_config.h
+++ src/config/user_config.h
@@ -70,9 +70,7 @@
  */
  
 // Albert: enabled extended sleep mode
-// #WIP for debug turned sleep off for now
-static const sleep_state_t app_default_sleep_mode = ARCH_SLEEP_OFF;
-// static const sleep_state_t app_default_sleep_mode = ARCH_EXT_SLEEP_ON;
+static const sleep_state_t app_default_sleep_mode = ARCH_EXT_SLEEP_ON;
 
 /*
  ****************************************************************************************
